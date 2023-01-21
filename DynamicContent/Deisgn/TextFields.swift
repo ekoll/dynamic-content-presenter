a936d7b2217206e312e6775bc1d49ce4715b0271
Ekrem <eko_1994@hotmail.com>
2023-01-21 21:01:45 +0300

    add default textview style

--- DynamicContent/Deisgn/TextFields.swift
+++ DynamicContent/Deisgn/TextFields.swift
@@ -5,4 +5,15 @@
 //  Created by Ekrem Duvarbasi on 21.01.2023.
 //
 
-import Foundation
+import UIKit
+
+extension UITextField {
+    
+    static var standard: UITextField {
+        let textField = UITextField()
+        textField.font = .systemFont(ofSize: 14)
+        textField.borderStyle = .roundedRect
+        
+        return textField
+    }
+}
