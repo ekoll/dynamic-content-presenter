d9b4289df6a224580375e8d628eee79eba9e7a41
Ekrem <eko_1994@hotmail.com>
2023-01-21 15:20:37 +0300

    Initial Commit

---
+++ DynamicContent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DynamicContent
+//
+//  Created by Ekrem Duvarbasi on 21.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
