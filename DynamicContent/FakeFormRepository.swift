d5040aeb3f8628f19123e22faa9796a081527f50
Ekrem <eko_1994@hotmail.com>
2023-01-22 15:09:21 +0300

    add FakeFormRepository

--- DynamicContent/FakeFormRepository.swift
+++ DynamicContent/FakeFormRepository.swift
@@ -1,8 +1,20 @@
 //
-//  MemoryFormRepository.swift
+//  FakeFormRepository.swift
 //  DynamicContent
 //
 //  Created by Ekrem Duvarbasi on 22.01.2023.
 //
 
 import Foundation
+
+class FakeFormRepository: FormRepository {
+    
+    func getForm() -> FormModel {
+        let string = """
+"""
+        
+        let data = string.data(using: .utf8)!
+        
+        return try! JSONDecoder().decode(FormModel.self, from: data)
+    }
+}
